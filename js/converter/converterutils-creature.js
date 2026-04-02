823fb97e51e82c7feab84919805cd422f730ebe2
TheGiddyLimit <TheGiddyLimit@5etools.com>
2026-04-02 13:01:43 +0100

    v1.215.0

--- js/converter/converterutils-creature.js
+++ js/converter/converterutils-creature.js
@@ -786,6 +786,7 @@ export class TraitActionTag {
 
 			"devil's sight": "Devil's Sight",
 			"devil sight": "Devil's Sight",
+			"ethereal sight": "Ethereal Sight",
 
 			"immutable form": "Immutable Form",
 
